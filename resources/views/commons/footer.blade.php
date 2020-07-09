4de579d5c1420dcee88b0d112194c04db6c26757
masaruijuin <masaruijuin@gmail.com>
2020-07-09 12:28:38 +0000

    トップページ画面作成

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="mt-5">
+  <nav class="navbar navbar-light bg-warning">
+    <div class="container">
+      <div class="navbar-brand">© 2020 Hakkutsu, All rights reserved.</div>
+      <ul class="navbar-nav">
+        <li class="nav-item"><a href="" class="nav-link">プライバシーポリシー</a></li>
+        <li class="nav-item"><a href="" class="nav-link">出店依頼はこちら</a></li>
+      </ul>
+    </div>
+  </nav>
+</footer>
