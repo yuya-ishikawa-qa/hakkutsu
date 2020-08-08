212adbc06e943f2a5bbc863e044a333bfde96f2c
shunsuke-katsuyama <shunsuke.kty@gmail.com>
2020-08-08 21:34:33 +0000

    レビュー機能実装コードレビュー修正3回目

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>HAKKUTSU 地域ブランド応援サイト</title>
-  <script src=" {{ asset('js/app.js') }} "></script>
+  <script src="{{ asset('js/app.js') }}"></script>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
