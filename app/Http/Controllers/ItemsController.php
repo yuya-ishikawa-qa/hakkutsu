85b1a060874b294209375cbbe15985221688f22c
ryota <ryota@example.com>
2020-08-08 07:00:07 +0000

    各コメント追記

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -27,6 +27,7 @@ class ItemsController extends Controller
 
     public function create($store_id)
     {
+        //情報を取得し、変数に代入
         $taxes = \App\Tax::orderBy('code','asc')->pluck('tax_rate', 'code');
         $user = \Auth::user();
         $store = Store::findOrFail($store_id);
@@ -38,25 +39,33 @@ class ItemsController extends Controller
             'items' => $items,
             'taxes' => $taxes,
         ];
+        
         return view('items.create',$data);
     }
 
     public function confirm(ItemRequest $request,$store_id)
     {
+        //情報を取得し、変数に代入
         $taxes = \App\Tax::orderBy('code','asc')->pluck('tax_rate', 'code');
         $store = Store::findOrFail($store_id);
+
+        //image_pathを除いた情報を変数に代入
         $post_data = $request->except('image_path');
+        //アップロードされた情報を変数に代入
         $path = $request->file('image_path');
+        //ファイルを指定した位置に保存し、変数に代入
         $temp_path = $path->store('public/temp');
         //str_replaceメソッドで、public/をstorage/に置き換え
         $read_temp_path = str_replace('public/', 'storage/', $temp_path);
 
+        //上記で取得した変数を代入
         $data = array(
             'temp_path' => $temp_path,
             'read_temp_path' => $read_temp_path, 
             'store' => $store,
             'taxes' => $taxes,
         );
+        //セッションに情報を保存
         $request->session()->put([
             'data' => $data,
             'post_data' => $post_data,
@@ -68,6 +77,7 @@ class ItemsController extends Controller
         }else{
             $post_data['tax_id'] = '10%';
         }
+
         return view('items.confirm')->with([
             'post_data' => $post_data,
             'data' => $data,
@@ -76,21 +86,27 @@ class ItemsController extends Controller
     // 完了フォーム
         public function store(Request $request)
         {
+            //セッションから必要なデータを取得
             $data = $request->session()->get('data');
             $post_data = $request->session()->get('post_data');
+            //上記のデータを変数に代入
             $store = $data['store'];
             $temp_path = $data['temp_path'];
             $read_temp_path = $data['read_temp_path'];
+
             //ファイル名は$temp_pathから"public/temp/を空白で除いたもの
             $filename = str_replace('public/temp/', '', $temp_path);
             //画像を保存するパスは"public/stores_image/xxx.jpeg"
             $storage_path = 'public/stores_image/'.$filename;
+
+            //dataのセッション情報を破棄
             $request->session()->forget('data');
             //Storageファサードのmoveメソッドで、第一引数->第二引数へファイルを移動
             Storage::move($temp_path, $storage_path);
             //publicをstorage/img/public/に置き換え、保存ファイルに移動
             $read_path = str_replace('public/', 'storage/', $storage_path);
 
+            //データベースへの保存処理
             $item = new Item();
             $item->image_path = $read_path;
             $item->item_name = $post_data['item_name'];
@@ -101,6 +117,7 @@ class ItemsController extends Controller
             $item->tax_id = $post_data['tax_id'];
             $item->store_id = $store->id;
             $item->save();
+
             return redirect()->route('stores.management')->with([
                 'flash_message' => '出品しました',
             ]);
@@ -108,16 +125,19 @@ class ItemsController extends Controller
 
         public function edit($store_id,$item_id)
         {   
+            //情報を取得し、変数に代入
             $taxes = \App\Tax::orderBy('code','asc')->pluck('tax_rate', 'code');
             $user = \Auth::user();
             $store = Store::findOrFail($store_id);
             $item = Item::findOrFail($item_id);
+
             $data=[
                 'user' => $user,
                 'store' => $store,
                 'item' => $item,
                 'taxes' => $taxes,
             ];
+
             return view('items.edit',$data);
         }
 
@@ -125,16 +145,22 @@ class ItemsController extends Controller
         {
                 // 画像がアップロードされたら保存
             if ($request->image_path) {
+                //ファイル情報を変数に代入
                 $path = $request->file('image_path');
+                //指定の場所にファイルを保存し、変数に代入
                 $storage_path = $path->store('public/stores_image');
-                //publicをstorage/img/public/に置き換え、保存ファイルに移動
+                //publicをstorageに置き換え、読み込み場所を変数に代入
                 $read_path = str_replace('public/', 'storage/', $storage_path);
+                //アイテム情報を取得
                 $item = Item::findOrFail($item_id);
+                //読み込み場所をデータベースに保存
                 $item->image_path = $read_path;
                 $item->save();
                 }
-                
+            
+            //image_pathを除いて変数に代入
             $post_data = $request->except('image_path');
+            //上記の変数を変数に代入
             $params = array(
                 'item_name' => $post_data['item_name'],
                 'status' => $post_data['status'],
@@ -143,8 +169,11 @@ class ItemsController extends Controller
                 'description' => $post_data['description'],
                 'tax_id' => $post_data['tax_id'],
             );  
+            //item情報を取得し、変数に代入
             $item = Item::findOrFail($item_id);
+            //データベースに保存
             $item->fill($params)->save();
+
             return redirect()->route('stores.management')->with([
                 'flash_message' => '商品を編集しました',
             ]);
@@ -152,9 +181,13 @@ class ItemsController extends Controller
 
         public function destroy($store_id,$item_id)
         {   
+            //情報を取得し、変数に代入
             $store = Store::findOrFail($store_id);
-            $item = Item::findOrFail($item_id);
+            $item = Item::findOrFail($item_id)
+            ;
+            //データベースから削除処理
             $item->delete();
+
             return redirect()->route('stores.management')->with([
                 'flash_message' => '商品を削除しました。',
             ]);
