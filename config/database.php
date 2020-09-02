23472b4294b1959a9e18b3a4214279815d512f26
ryota <ryota@example.com>
2020-09-02 12:57:30 +0000

    database.phpの設定

--- config/database.php
+++ config/database.php
@@ -71,7 +71,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
