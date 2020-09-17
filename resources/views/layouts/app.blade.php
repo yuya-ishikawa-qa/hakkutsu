83bd444be976443d27c70766799fdcf720506370
ryota <ryota@example.com>
2020-09-17 14:17:40 +0000

    画像調整

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,17 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>HAKKUTSU 地域ブランド応援サイト</title>
-  <style type="text/css">
-  @if(app('env') == 'production')
-    <script src="{{ secure_asset('js/app.js') }}"></script>
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
-@else
   <script src="{{ asset('js/app.js') }}"></script>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-@endif
-
+  <style type="text/css">
     <!--
     button.stripe-button-el,
     button.stripe-button-el>span {
